33b15376390cbd1ec284fe697b1522bfd015e955
Mateohr123 <j.hernandez@coadvil.edu.co>
2025-02-20 17:20:30 -0500

    Update Ejercicio 6.py

---
+++ Ejercicio 6.py
@@ -0,0 +1,9 @@
+numero = int(input("Ingrese un número de tres digitos: "))
+
+digito1 = numero % 10  
+digito2 = (numero // 10) % 10  
+digito3 = numero // 100  
+
+numero_invertido = digito1 * 100 + digito2 * 10 + digito3
+
+print(f"El número {numero} al revés es: {numero_invertido}")
