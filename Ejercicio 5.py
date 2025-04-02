73e7e6a50d7c9116fea194760a180f429a1e0e98
Mateohr123 <j.hernandez@coadvil.edu.co>
2025-04-02 17:50:03 -0500

    Update Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,3 +1,3 @@
-centimetros = float(input("ingresa la cantidad de centimetros que quieres pasar a pulgadas:"))
+centimetros = float(input("ingresa la cantidad de centimetros que quiera convertir a pulgadas:"))
 dividir = centimetros / 2.54
 print("la cantidad de centimetros convertida a pulgadas es:",dividir)
